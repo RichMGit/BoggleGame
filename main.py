db9271c648b780707d010eecfcb2bf869269a9d2
Tom Hill <tomhill@notonthehighstreet.com>
2016-04-01 16:30:41 +0100

    We did a lot

--- main.py
+++ main.py
@@ -1,65 +1,82 @@
 import random
 
-cube_one = ['a', 'a', 'e', 'e', 'g', 'n']
-cube_two = ['a', 'b', 'b', 'j', 'o', 'o']
-cube_three = ['a', 'c', 'h', 'o', 'p', 's']
-cube_four = ['a', 'f', 'f', 'k', 'p', 's']
-cube_five = ['b', 'm', 'u', 'e', 'z', 'b']
-cube_six = ['i', 'c', 'u', 'j', 'c', 'c']
-cube_seven = ['d', 'k', 'j', 'a', 'd', 'd']
-cube_eight = ['a', 'y', 'e', 'e', 'v', 'b']
-cube_nine = ['s', 'u', 'w', 'e', 'c', 'n']
-cube_ten = ['d', 'n', 'e', 'e', 'x', 'n']
-cube_eleven = ['a', 'a', 'e', 'w', 'g', 'n']
-cube_twelve = ['a', 'a', 'x', 'e', 'g', 'n']
-cube_thirteen = ['l', 'y', 'e', 'o', 'o', 'n']
-cube_fourteen = ['q', 'w', 'f', 'y', 'p', 'n']
-cube_fifteen = ['a', 's', 'w', 'a', 'g', 'n']
-cube_sixteen = ['a', 't', 'y', 'u', 'i', 'n']
+cubes = [
+    ['a', 'a', 'e', 'e', 'g', 'n'],
+    ['a', 'b', 'b', 'j', 'o', 'o'],
+    ['a', 'c', 'h', 'o', 'p', 's'],
+    ['a', 'f', 'f', 'k', 'p', 's'],
+    ['b', 'm', 'u', 'e', 'z', 'b'],
+    ['i', 'c', 'u', 'j', 'c', 'c'],
+    ['d', 'k', 'j', 'a', 'd', 'd'],
+    ['a', 'y', 'e', 'e', 'v', 'b'],
+    ['s', 'u', 'w', 'e', 'c', 'n'],
+    ['d', 'n', 'e', 'e', 'x', 'n'],
+    ['a', 'a', 'e', 'w', 'g', 'n'],
+    ['a', 'a', 'x', 'e', 'g', 'n'],
+    ['l', 'y', 'e', 'o', 'o', 'n'],
+    ['q', 'w', 'f', 'y', 'p', 'n'],
+    ['a', 's', 'w', 'a', 'g', 'n'],
+    ['a', 't', 'y', 'u', 'i', 'n']
+]
+
+for cube in cubes:
+    random.shuffle(cube)
+
+random.shuffle(cubes)
 
 game_letters = []
 
-random.shuffle(cube_one)
-random.shuffle(cube_two)
-random.shuffle(cube_three)
-random.shuffle(cube_four)
-random.shuffle(cube_five)
-random.shuffle(cube_six)
-random.shuffle(cube_seven)
-random.shuffle(cube_eight)
-random.shuffle(cube_nine)
-random.shuffle(cube_ten)
-random.shuffle(cube_eleven)
-random.shuffle(cube_twelve)
-random.shuffle(cube_thirteen)
-random.shuffle(cube_fourteen)
-random.shuffle(cube_fifteen)
-random.shuffle(cube_sixteen)
+for cube in cubes:
+    game_letters.append(cube[0])
 
-
-game_letters.append(cube_one[0])
-game_letters.append(cube_two[0])
-game_letters.append(cube_three[0])
-game_letters.append(cube_four[0])
-game_letters.append(cube_five[0])
-game_letters.append(cube_six[0])
-game_letters.append(cube_seven[0])
-game_letters.append(cube_eight[0])
-game_letters.append(cube_nine[0])
-game_letters.append(cube_ten[0])
-game_letters.append(cube_eleven[0])
-game_letters.append(cube_twelve[0])
-game_letters.append(cube_thirteen[0])
-game_letters.append(cube_fourteen[0])
-game_letters.append(cube_fifteen[0])
-game_letters.append(cube_sixteen[0])
+grid = []
+a = 0
+for x in range(4):
+    grid.append(game_letters[a:a+4])
+    a += 4
 
 print(game_letters)
-word_one = raw_input('Enter a word: ')
-word_split = list(word_one)
-print(word_split)
-if (word_split[0] in game_letters):
-    fl = game_letters.index(word_split[0])
-    print (fl)
-elif (word_split[0] not in game_letters):
-    print ('letter not found')
+print(grid)
+
+
+def is_next_letter_valid(first, second):
+    # Each argument is an array eg [0][0]
+    # Are the indexes of the two letters within 1 of each other?
+    if -1 <= (first[0] - second[0]) <= 1 and -1 <= (first[1] - second[1]) <= 1:
+        return True
+    else:
+        return False
+
+
+
+def find_letter_coordinates(letter):
+    coordinate_store = []
+    # Given a letter return a list of all the coordinates that contain that letter
+    for row in range(4):
+        for column in range(4):
+            if grid[row][column] == letter:
+                coordinate_store.append([row, column])
+    return coordinate_store
+
+def remove_invalid_coordinates(coordinate_list, neighbour):
+    for coordinate in coordinate_list:
+        if not is_next_letter_valid(coordinate, neighbour):
+            coordinate_list.remove(coordinate)
+    return coordinate_list
+
+if is_next_letter_valid([0, 0], [0, 1]):
+    print('This is ok')
+
+user_word = input('Enter a word: ')
+
+print(find_letter_coordinates(user_word[0]))
+print(remove_invalid_coordinates([[1, 1], [2, 2], [4, 4]], [1, 2]))
+
+# word_one = input('Enter a word: ')
+# word_split = word_one
+# print(word_split)
+# if word_split[0] in game_letters:
+#     fl = game_letters.index(word_split[0])
+#     print(fl)
+# elif word_split[0] not in game_letters:
+#     print('letter not found')
