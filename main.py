df493d403196866e2baf7af9109652145b35db7e
Rich Matthews <richmatthews@notonthehighstreet.com>
2016-04-06 17:00:08 +0100

    Functionality 90% complete

--- main.py
+++ main.py
@@ -50,62 +50,81 @@ def is_next_letter_valid(first, second):
         return False
 
 
-def find_letter_coordinates(letter):
+def find_letter_coordinates(letter, list_of_valid_coordinates_of_the_previous_letter_in_the_word=None):
     coordinate_store = []
     # Given a letter return a list of all the coordinates that contain that letter
     for row in range(4):
         for column in range(4):
             if grid[row][column] == letter:
                 coordinate_store.append([row, column])
-    return coordinate_store
+    if list_of_valid_coordinates_of_the_previous_letter_in_the_word is None:
+        return coordinate_store
+    else:
+        for coordinate in coordinate_store:
+            coord_is_valid = False
+            for one_of_the_valid_coordinates_of_the_previous_letter_in_the_word in list_of_valid_coordinates_of_the_previous_letter_in_the_word:
+                if is_next_letter_valid(one_of_the_valid_coordinates_of_the_previous_letter_in_the_word, coordinate):
+                    coord_is_valid = True
+            if coord_is_valid is False:
+                coordinate_store.remove(coordinate)
+        return coordinate_store
 
 
-def remove_invalid_coordinates(coordinate_list, neighbour):
-    for coordinate in coordinate_list:
-        if not is_next_letter_valid(coordinate, neighbour):
-            coordinate_list.remove(coordinate)
-    return coordinate_list
+def is_word_on_board(word):
+    index = 0
+    coordinates = find_letter_coordinates(user_word[index])
+    while len(coordinates) > 0:
+        if index == len(word) -1:
+            return True
+        index += 1
+        coordinates = find_letter_coordinates(user_word[index], coordinates)
+    return False
 
 
 def print_grid(grid):
     for row in grid:
         print(' '.join(row))
 
-
-def unknown(user_word):
-    for index, letter in enumerate(user_word):
-        for s in find_letter_coordinates(user_word[index]):
-            if index == len(user_word) - 1:
-                return False
-            for a in find_letter_coordinates(user_word[index + 1]):
-                if is_next_letter_valid(s, a):
-                    print(s, a, 'are valid')
-                    return True
+def is_word_in_word_list(word):
+    for valid_word in open("word_list.txt"):
+        if word.rstrip() == valid_word.rstrip():
+            return True
     return False
 
+def is_word_valid(word):
+    if len(word) <= 2:
+        return False
+    if is_word_in_word_list(word) is False:
+        return False
+    if is_word_on_board(word) is False:
+        return False
+    return True
+
+def word_score(word):
+    if len(word) == 3 or len(word) == 4:
+        return 1
+    elif len(word) == 5:
+        return 2
+    elif len(word) == 6:
+        return 3
+    elif len(word) == 7:
+        return 5
+    elif len(word) >= 8:
+        return 11
+    else:
+        return 0
+
+# Create coords lists for each letter
+# Function needed that is passed two lists of coordinates, returns the lists minus any that aren't valid with each other
+# Function to iterate through the word
 grid = create_grid(dice)
 print_grid(grid)
 
-if is_next_letter_valid([0, 0], [0, 1]):
-    print('This is ok')
-
-user_word = input('Enter a word: ')
-print(unknown(user_word))
-
-
-
-# for index in len(user_word):
-#     for s in find_letter_coordinates(user_word[index]):
-#         for a in find_letter_coordinates(user_word[1]):
-#             if is_next_letter_valid(s, a):
-#                 for p in find_letter_coordinates(user_word[2]):
-#                     if is_next_letter_valid(a, p):
-#                         return True
-
-
-
-# first_letter_coordinates = find_letter_coordinates(user_word[0])
-#
-#
-# print(remove_invalid_coordinates(first_letter_coordinates, [1, 2]))
-
+score = 0
+for _ in range(5):
+    user_word = input('Enter a word: ')
+    if is_word_valid(user_word):
+        score += word_score(user_word)
+        print ("Correct. New score: ", str(score))
+    else:
+        print ("Invalid word")
